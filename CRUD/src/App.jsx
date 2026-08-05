e58b57b85468886af2071d5f1572498368d96f80
Harsh Chinchkhede <hchichkhede100@gmail.com>
2026-08-05 16:01:08 +0530

    new

--- CRUD/src/App.jsx
+++ CRUD/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Create from "./Components/Create";
 
 
 const App = () => {
   return (
     <>
-      
+       <create></create>
        <h1>We are going to perform CRUD operations</h1>
        <button type="button" class="btn btn-primary">Primary</button>
+        
     </>
      
   );
